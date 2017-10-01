2227014b99aec16027db5d24ca4a8500bd4172e5
John Tuttle <thejwt@gmail.com>
2017-10-01 09:10:43 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,6 +1,13 @@
 class Patient
+  attr_reader :name
+  attr_accessor :appointments
 
   def initialize(name)
     @name = name
+    @appointments = []
+  end
+
+  def add_appointment
+    
   end
 end
